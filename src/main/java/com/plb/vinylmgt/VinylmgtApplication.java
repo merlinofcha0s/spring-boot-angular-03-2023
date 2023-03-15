d5150ae93d3bc8aa45533df5a6d31f4d95a13018
Cyril Casaucau <casaucau.cyril@protonmail.com>
2023-03-15 13:36:31 +0100

    First commit

---
+++ src/main/java/com/plb/vinylmgt/VinylmgtApplication.java
@@ -0,0 +1,14 @@
+package com.plb.vinylmgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylmgtApplication {
+
+	public static void main(String[] args) {
+		System.out.println("test devtool");
+		SpringApplication.run(VinylmgtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plb/vinylmgt/VinylmgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plb.vinylmgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinylmgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
